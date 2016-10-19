612a68d9ff01614dccb649cc5513dacf17f0b6d5
alextishin <alextishin91@gmail.com>
2016-10-19 14:14:43 +0300

    class Tree (methods isBranchOpen, createOpenedMap)

--- assets/js/components/Tree/index.js
+++ assets/js/components/Tree/index.js
@@ -7,22 +7,56 @@ class Tree extends Component {
     super(props);
 
     this.state = {
-      collapsed: false
+      collapsed: false,
+      data: props.data.map((item) => {
+        if(!item.id) {
+          return  {
+            id: shortid.generate(),
+            ...item
+          };
+        }
+
+
+        return {...item};
+
+      }),
+      openedMap: null
     }
+
   }
 
   static propTypes = {
     data: React.PropTypes.array.isRequired
   }
 
+  componentWillMount = () => {
+    this.setState({
+      openedMap: this.createOpenedMap(this.state.data)
+    }, ()=>{console.log(this.state)})
+  }
+
+  createOpenedMap = (data) => {
+    var openNodeMap = {};
+
+    data.forEach((item) => {
+      openNodeMap[item.id] = false;
+    });
+
+    return openNodeMap;
+  }
+
+  isBranchOpen = (branchId) => {
+    return this.state.openedMap[branchId];
+  }
+
   isCollapsed = () => {
     return this.state.collapsed;
   }
 
-  renderCollapseIcon = () => {
+  renderBranchOpenIcon = (branchId) => {
     return (
       <span className="tree__plus">
-        <i className={`fa fa-plus-square-o`}></i>
+        <i className={`fa fa-${ this.isBranchOpen(branchId) ? 'minus-square-o' : 'plus-square-o' }`}></i>
       </span>
     )
   }
@@ -37,15 +71,15 @@ class Tree extends Component {
         </li>
       )
     });
-    
+
     return <ul>{branchLeafs}</ul>;
   }
 
   renderBranches = () => {
-    return this.props.data.map((branch) => {
+    return this.state.data.map((branch) => {
       return (
         <li key={shortid.generate()} className='tree__branch'>
-          {branch.data ? this.renderCollapseIcon() : ''}
+          {branch.data ? this.renderBranchOpenIcon(branch.id) : ''}
           <span className={`tree__folder ${!branch.data ? 'tree__folder--empty' : ''}`}>
             <i className={`fa fa-${this.isCollapsed() ? 'folder-open' : 'folder'}`}></i>
           </span>
